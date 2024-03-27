d39f13a5eb05fb9fcee6c677eee47e8140fecf9a
tailor.X <64458564+tailorx@users.noreply.github.com>
2024-03-28 02:52:59 +0800

    Update sidebar.tsx
    修改侧边名字

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          TOEFL Reading Teaching Assistant
+          MetaCogLite- your TOEFL Reading TA
         </div>
         <div className={styles["sidebar-sub-title"]}>
           -----By Yizheng Liu.
